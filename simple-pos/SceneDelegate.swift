0e4b482c57d52566a60bff47af94e856b19e6ad2
Wayne Carter <wayneacarter@hotmail.com>
2024-05-23 23:37:36 -0400

    Clean up obsolete code from ViewController2

--- simple-pos/SceneDelegate.swift
+++ simple-pos/SceneDelegate.swift
@@ -10,21 +10,12 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    // Pasin, to run the scene from the story board, comment out this
-    // method and uncomment the once just below it.
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = ViewController()
         self.window?.makeKeyAndVisible()
     }
-    
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-//        guard let _ = (scene as? UIWindowScene) else { return }
-//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
