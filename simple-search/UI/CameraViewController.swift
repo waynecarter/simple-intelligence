b76fd1ce06055314935c5ade238f0619da691429
Wayne Carter <wayneacarter@hotmail.com>
2024-06-30 15:54:18 -0400

    Remove blur view

--- simple-search/UI/CameraViewController.swift
+++ simple-search/UI/CameraViewController.swift
@@ -10,16 +10,15 @@ import AVFoundation
 import Combine
 
 class CameraViewController: ProductsViewController {
-    @IBOutlet weak var blurEffectView: UIVisualEffectView!
     @IBOutlet weak var explainerView: UIView!
     
     private let camera: Camera = .shared
     
-    private let tabBarBackgroundColor = UIColor.systemBackground.withAlphaComponent(0.7)
-    
     private var cancellables = Set<AnyCancellable>()
     
     private func setup() {
+        view.backgroundColor = .secondarySystemBackground
+        
         // Check the camera authorization and display the explainer if needed
         updateCameraAuthorization()
         
@@ -35,7 +34,6 @@ class CameraViewController: ProductsViewController {
             .sink { [weak self] products in
                 self?.products = products
                 if products.count > 0 {
-                    self?.camera.stop()
                     Haptics.shared.generateSelectionFeedback()
                 }
             }.store(in: &cancellables)
@@ -45,15 +43,43 @@ class CameraViewController: ProductsViewController {
             .sink { [weak self] authorized in
                 self?.updateCameraAuthorization(authorized)
             }.store(in: &cancellables)
-        
-        camera.start()
     }
     
     private func style() {
         tabBarController?.overrideUserInterfaceStyle = .dark
+        updateStyleForProducts()
+    }
+    
+    private func updateStyleForProducts() {
+        func updateStyleForCamera(isRunning: Bool) {
+            if isRunning {
+                tabBarController?.tabBar.backgroundColor = .black.withAlphaComponent(0.7)
+            } else {
+                tabBarController?.tabBar.backgroundColor = nil
+            }
+        }
         
-        if blurEffectView.alpha == 0 {
-            tabBarController?.tabBar.backgroundColor = tabBarBackgroundColor
+        if products.count > 0 {
+            if camera.isRunning {
+                // Stop the camera
+                camera.stop()
+                // Hide the preview
+                camera.preview.hide(animations: {
+                    updateStyleForCamera(isRunning: false)
+                })
+            }
+        } else {
+            if camera.isRunning == false {
+                // Hide the preview
+                camera.preview.hide(completion: {
+                    // Start the camera
+                    self.camera.start()
+                    // Show the preview
+                    self.camera.preview.show(animations: {
+                        updateStyleForCamera(isRunning: true)
+                    })
+                })
+            }
         }
     }
     
@@ -76,28 +102,13 @@ class CameraViewController: ProductsViewController {
     
     override var products: [Database.Product] {
         didSet {
+            // When the products change, update the styling
             guard oldValue != products else { return }
-            
-            if products.count > 0 {
-                UIView.animate(withDuration: 0.2) {
-                    self.blurEffectView.alpha = 1
-                    self.tabBarController?.tabBar.backgroundColor = .clear
-                }
-            } else {
-                camera.preview.hide(completion: {
-                    self.camera.start()
-                    self.camera.preview.show(animations: {
-                        self.blurEffectView.alpha = 0
-                        self.tabBarController?.tabBar.backgroundColor = self.tabBarBackgroundColor
-                    })
-                })
-            }
+            updateStyleForProducts()
         }
     }
     
     // MARK: - View Lifecycle
-    
-    private var cameraWasRunning = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -106,30 +117,13 @@ class CameraViewController: ProductsViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Apply styles
         style()
-        
-        // If the camera was running when the view disappeared, start it again
-        if cameraWasRunning {
-            camera.start()
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        // If the camera is enabled, show it
-        if camera.authorized {
-            camera.preview.show()
-        }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        // If the camera is running, stop it and note whether it was running so we can restart it when the view re-appears
-        cameraWasRunning = camera.isRunning
+        // If the camera is running, stop it
         if camera.isRunning {
             camera.stop()
             camera.preview.hide()
