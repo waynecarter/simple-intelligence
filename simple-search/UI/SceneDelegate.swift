c24bb0938c792101f451e4144f3fb7dbd862e82b
Wayne Carter <wayneacarter@hotmail.com>
2024-07-03 13:39:57 -0400

    Add comment

--- simple-search/UI/SceneDelegate.swift
+++ simple-search/UI/SceneDelegate.swift
@@ -22,8 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private var cancellables = Set<AnyCancellable>()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Get the window from the scene
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
