ae7c78750dfcf87f61eb6e21d74314598f184ae0
Wayne Carter <wayneacarter@hotmail.com>
2024-07-03 22:54:12 -0400

    Simplify search bar spacing handling

--- simple-search/UI/TextViewController.swift
+++ simple-search/UI/TextViewController.swift
@@ -11,7 +11,6 @@ import Combine
 class TextViewController: ProductsViewController, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchBar_TopConstraint: NSLayoutConstraint!
-    var searchBar_TopConstraint_Constant: CGFloat = 0
     
     @IBOutlet weak var explainerView: UIView!
     
@@ -22,16 +21,11 @@ class TextViewController: ProductsViewController, UISearchBarDelegate {
         searchBar.placeholder = "Produce, Bakery, Dairy, and More"
         searchBar.backgroundImage = UIImage()
         searchBar.searchTextField.addTarget(self, action: #selector(searchDidChange(_:)), for: .editingChanged)
-        searchBar_TopConstraint_Constant = searchBar_TopConstraint.constant
     }
     
     private func style() {
         tabBarController?.overrideUserInterfaceStyle = .unspecified
         tabBarController?.tabBar.backgroundColor = .clear
-    }
-    
-    override func viewSafeAreaInsetsDidChange() {
-        super.viewSafeAreaInsetsDidChange()
     }
     
     // MARK: - View Lifecycle
@@ -60,24 +54,32 @@ class TextViewController: ProductsViewController, UISearchBarDelegate {
     
     private func beginSearch() {
         searchBar.setShowsCancelButton(true, animated: true)
+        updateSearchBar(isSearching: true)
+    }
+    
+    private func updateSearchBar(isSearching: Bool) {
+        // If there is no space at the top of the screen, we need some to inset the search bar
+        let needsSpacing = isSearching && (navigationController?.view.safeAreaInsets.top == 0)
         
-        // If there is no space at the top of the screen, add some to inset the search bar
-        if let safeAreaInsets = navigationController?.view.safeAreaInsets, safeAreaInsets.top > 0 {
-            searchBar_TopConstraint.constant = 0
+        if needsSpacing {
+            self.searchBar_TopConstraint.constant = 12
+            UIView.animate(withDuration: UINavigationController.hideShowBarDuration) {
+                self.view.layoutIfNeeded()
+            }
         } else {
-            searchBar_TopConstraint.constant = 12
+            self.searchBar_TopConstraint.constant = 0
+            self.view.layoutIfNeeded()
         }
     }
     
     private func endSearch(updateExplainer: Bool = true) {
+        updateSearchBar(isSearching: false)
+        
         navigationController?.setNavigationBarHidden(false, animated: true)
         
         searchBar.resignFirstResponder()
         searchBar.setShowsCancelButton(false, animated: true)
         searchBar.text = nil
-        
-        // Reset to the original spacing
-        searchBar_TopConstraint.constant = searchBar_TopConstraint_Constant
         
         // Clear the products
         products = []
@@ -96,18 +98,23 @@ class TextViewController: ProductsViewController, UISearchBarDelegate {
         beginSearch()
     }
     
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        // Enable the cancel button when the search bar is not the first responder
+        if let cancelButton = searchBar.value(forKey: "cancelButton") as? UIButton {
+            // Enable the button sync and async to catch all cases where the button is disabled
+            cancelButton.isEnabled = true
+            DispatchQueue.main.async {
+                cancelButton.isEnabled = true
+            }
+        }
+    }
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         endSearch()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        // Dismiss the keyboard
         searchBar.resignFirstResponder()
-        
-        // Make sure the cancel button stays enabled even when the search bar is not the first responder
-        if let cancelButton = searchBar.value(forKey: "cancelButton") as? UIButton {
-            cancelButton.isEnabled = true
-        }
     }
     
     // MARK: - Actions
