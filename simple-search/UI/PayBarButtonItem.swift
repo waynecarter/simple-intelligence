96c5169936b356b1979ebcadeb8f46861c0dfd8a
Wayne Carter <wayneacarter@hotmail.com>
2024-07-03 22:51:23 -0400

    Add top and bottom padding to the pay button so it will fit in with standard spacing

--- simple-search/UI/PayBarButtonItem.swift
+++ simple-search/UI/PayBarButtonItem.swift
@@ -20,16 +20,29 @@ class PayBarButtonItem: UIBarButtonItem {
     private func setup() {
         total = 0
         
+        let buttonContainer = UIView()
+        buttonContainer.translatesAutoresizingMaskIntoConstraints = false
+        
         button.configuration = UIButton.Configuration.filled()
         button.configuration?.buttonSize = .large
         button.configuration?.baseBackgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.addAction(UIAction(title: "Pay") { [weak self] _ in
             if let target = self?.target, let action = self?.action {
                 _ = target.perform(action, with: self)
             }
         }, for: .touchUpInside)
+        buttonContainer.addSubview(button)
         
-        self.customView = button
+        // Add padding to fit in with standard spacing
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: buttonContainer.topAnchor, constant: 2),
+            button.leftAnchor.constraint(equalTo: buttonContainer.leftAnchor),
+            button.rightAnchor.constraint(equalTo: buttonContainer.rightAnchor),
+            button.bottomAnchor.constraint(equalTo: buttonContainer.bottomAnchor, constant: -2)
+        ])
+        
+        self.customView = buttonContainer
     }
     
     var total: Double = .zero {
